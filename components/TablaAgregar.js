7382f91c1f938fd3eddea532381ed8dae51f95e5
naomo123 <55609446+naomo123@users.noreply.github.com>
2021-11-07 12:43:41 -0600

    Incompleto, y está el formulario para ser llenado

--- components/TablaAgregar.js
+++ components/TablaAgregar.js
@@ -1,45 +1,62 @@
-import React, {Component} from 'react';
+import * as React from 'react';
+import { DataTable } from 'react-native-paper';
+import Database from './DataBase';
+import { useNavigation } from "@react-navigation/core";
 import {View, Text, StyleSheet, TouchableHighlight, ScrollView} from 'react-native';
-import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
+import Habitaciones from './Habitaciones';
 
-export default class TablaAgregar extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            header: ['Tipo de habitacion','Precio', '# Noches'],
-            item: [
-                ["HP 1", "$ 350.00", "2"],
-                ["HE 1", "$ 150.00", "2"],
-                ["HM 2", "$ 200.00", "2"]
-            ]
-        }
-    }
+const TablaAgregar = () => {
+   
+  const navigation = useNavigation("");
+  return (
+    <View style={styles.content}>
+    <ScrollView>
+        <Text style={styles.titulo}>HABITACIONES SELECCIONADAS</Text>
+        <View>
+    <DataTable>
+      <DataTable.Header>
+        <DataTable.Title>Tipo de habitacion</DataTable.Title>
+        <DataTable.Title numeric>Precio</DataTable.Title>
+        <DataTable.Title numeric>Noches</DataTable.Title>
+      </DataTable.Header>
 
-    render(){
-        const state = this.state;
-        return(
-            <View style={styles.content}>
-                <ScrollView>
-                    <Text style={styles.titulo}>HABITACIONES SELECCIONADAS</Text>
-                    <View>
-                        <Table borderStyle={{borderWidth: 2, borderColor: '#fff'}}>
-                            <Row data={state.header} style={styles.head} textStyle={{color: "#fff", textAlign: 'center', fontSize: 17}}/>
-                            <Rows  data={state.item} style={styles.tbody} textStyle={styles.text}/>
-                        </Table>
-                    </View>
-                    <TouchableHighlight style={styles.btn_confirm}>
-                        <Text style={{fontWeight: 'bold', fontSize: 25, textAlign: 'center'}}>
+      <DataTable.Row>
+        <DataTable.Cell>Frozen yogurt</DataTable.Cell>
+        <DataTable.Cell numeric>159</DataTable.Cell>
+        <DataTable.Cell numeric>6.0</DataTable.Cell>
+      </DataTable.Row>
+
+      <DataTable.Row>
+        <DataTable.Cell>Ice cream sandwich</DataTable.Cell>
+        <DataTable.Cell numeric>237</DataTable.Cell>
+        <DataTable.Cell numeric>8.0</DataTable.Cell>
+      </DataTable.Row>
+
+    
+    </DataTable>
+    </View>
+    <TouchableHighlight style={styles.btn_confirm}
+    onPress={()=> navigation.navigate("DatosOrden")}
+    
+    >
+                        <Text style={{ fontSize: 25, textAlign: 'center'}}>
                             HACER RESERVA
                         </Text>
                     </TouchableHighlight>
-                    <Text style={styles.btn_cancel}>
+
+                    <TouchableHighlight style={styles.btn_cancel}
+    onPress={()=> navigation.navigate("Habitaciones")}
+    
+    >
+                    <Text>
                         CANCELAR PROCESO
                     </Text>
+                    </TouchableHighlight>
                 </ScrollView>
             </View>
-        );
-    }
-}
+  );
+};
+
 
 const styles = StyleSheet.create({
     content: {
@@ -68,17 +85,7 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         fontSize: 24
     },
-    itemContainer: {
-        textAlign: 'center',
-        justifyContent: 'flex-end',
-        borderRadius: 5,
-        padding: 10,
-        height: 100,
-        backgroundColor: "ivory"
-    },
-    head: { height: 60, backgroundColor: '#000', width: 350},
-    tbody: {
-        height: 60, backgroundColor: "ivory", borderColor: "#001000", width: 350
-    },
-    text: { margin: 6, fontSize: 20, textAlign: 'center' }
+   
+  
 })
+export default TablaAgregar;
