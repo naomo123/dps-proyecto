75b0250779d108114f6ed4b079a30c14be6cc3df
naomo123 <55609446+naomo123@users.noreply.github.com>
2021-11-07 12:44:26 -0600

    update firebase

--- components/firebase.js
+++ components/firebase.js
@@ -1,4 +1,6 @@
-import firebase from "firebase";
+
+import firebase from 'firebase';
+import firestore from 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyDNKnVgPip8LlZQqYgrcdS9CFN37SpcZzE",
@@ -11,12 +13,22 @@ const firebaseConfig = {
 };
 
 let app;
+
 if(firebase.apps.length ===0){
+
   app=firebase.initializeApp(firebaseConfig);
 } else {
   app=firebase.app()
 }
 const provider = new firebase.auth.GoogleAuthProvider();
 
-const  auth=firebase.auth()
-export {auth, provider};
+const  auth=firebase.auth();
+
+firebase.firestore(); 
+
+const settings = { timestampsInSnapshots : true , } ;
+   
+  
+
+export {auth, provider,};
+export default firebase;  
