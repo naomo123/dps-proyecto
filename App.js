77db50c7807bb78975595e4d992ed345460ba307
naomo123 <55609446+naomo123@users.noreply.github.com>
2021-11-07 12:47:19 -0600

    Nuevas pantallas, captura del usuario
    Se captura el usuario que ha iniciado sesión

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-
-
-// Importing combination
-import React from 'react';
+import React, {useState,createContext,useEffect} from 'react';
 import { StyleSheet,  View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-    
+  
 import Inicio from './components/Inicio';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import Perfil from './components/Perfil';
 import Habitaciones from './components/Habitaciones';
+import TablaAgregar from './components/TablaAgregar';
+import DatosOrden from './components/DatosOrden';
 
 const Stack=createNativeStackNavigator();
+
+export const RoomContext = React.createContext();
+export const UserContext = React.createContext();
+
 export default function App() {
+  const [user1,setUser1]=useState({
+    name:"",
+    email:"",
+    state:false
+  });
+  console.log(user1);
+
+
   return (
+    <UserContext.Provider value={[user1,setUser1]}>
     <NavigationContainer>
       <Stack.Navigator>
       <Stack.Screen
@@ -44,11 +56,21 @@ export default function App() {
          name="Habitaciones"
          component={Habitaciones}
          />
+ <Stack.Screen
+          options={{headerShown:false}}
+         name="TablaAgregar"
+         component={TablaAgregar}
+         />
 
-
+<Stack.Screen
+          options={{headerShown:false}}
+         name="DatosOrden"
+         component={DatosOrden}
+         />
          
       </Stack.Navigator>
     </NavigationContainer>
+    </UserContext.Provider>
   );
 }
 
